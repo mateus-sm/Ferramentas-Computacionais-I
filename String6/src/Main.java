38af0dd1f502f881aa4193e0ae03086a561539ef
Mateus Maciel <matteus6537@hotmail.com>
2024-09-17 21:28:13 -0300

    ADD Exs String

--- String6/src/Main.java
+++ String6/src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
     static String gerarPalavra() {
         char[] consoantes = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
