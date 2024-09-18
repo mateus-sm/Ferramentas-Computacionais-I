0be0d1eb40075abee6da02b0c2b3034606161f4b
Mateus Maciel <matteus6537@hotmail.com>
2024-09-17 21:42:39 -0300

    ADD Exs String

--- String3/src/Main.java
+++ String3/src/Main.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String texto, textos[];
-        int count = 0;
+        String texto;
 
         System.out.print("Digite seu texto: ");
         texto = sc.nextLine();
 
+        int count = 0;
         for (int i = 0; i < texto.length(); i++) {
             count++;
         }
-        System.out.printf("A palavra tem %d caracteres\n", count);
+        System.out.printf("O texto tem %d caracteres\n", count);
 
-        textos = texto.split(" ");
-        System.out.println(textos);
+        String[] palavras = texto.trim().split("\\s+");
+        int numPalavras = palavras.length;
+
+        System.out.printf("O texto tem %d palavras\n", numPalavras);
     }
 }
